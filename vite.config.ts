b19e3f8ebf612155975d258937d88c723566054c
Krryxa <krryxq@163.com>
2021-01-25 18:14:15 +0800

    feat: 增加配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,17 @@ const { resolve } = require('path')
  * @type {import('vite').UserConfig}
  */
 export default {
+  base: './',
+  open: true,
   alias: {
     '@': resolve(__dirname, 'src')
+  },
+  proxy: {
+    // '/api': {
+    //   target: 'https://blog.csdn.net/weixin_45292658',
+    //   changeOrigin: true,
+    //   rewrite: path => path.replace(/^\/api/, '')
+    // }
   },
   plugins: [vue()]
 }
